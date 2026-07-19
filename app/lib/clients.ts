329aad780b7c480e573bfd0c234afdb1ee202a05
santiagowhustt <santiagogestor6@gmail.com>
2026-07-19 01:23:04 -0300

    Update clients.ts

--- app/lib/clients.ts
+++ app/lib/clients.ts
@@ -6,4 +6,14 @@ export interface Client {
 export const clients: Client[] = [
   { name: "Anderson Zat", accountId: "act_1209746600428777" },
   { name: "Perondi", accountId: "act_629418268865871" },
+  { name: "7Agro", accountId: "act_2868078256818568" },
+  { name: "00 - DeuLaudo", accountId: "act_1474628546706407" },
+  { name: "Dr. Daniel", accountId: "act_738786292616470" },
+  { name: "CDI Umuarama", accountId: "act_2006105313532606" },
+  { name: "Cleiton Pooter - By Hp", accountId: "act_1113307093299103" },
+  { name: "Conexão Diagnóstica", accountId: "act_2357713678085694" },
+  { name: "Dr Eduardo", accountId: "act_883147101200876" },
+  { name: "Hotel Tirolesa", accountId: "act_1436531086924036" },
+  { name: "MedImpulso", accountId: "act_1621014168932865" },
+  { name: "SC Massagem", accountId: "act_356484881871886" },
 ];
